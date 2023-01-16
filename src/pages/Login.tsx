6f1f75848a0089af8e17c8a49ee6f21d4957d2ee
anry <anryclass@gmail.com>
2023-01-16 19:04:37 +0200

    feat(login-page): done password visibility toggle

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,10 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useForm } from 'react-hook-form';
-import { Stack, Container } from '@mui/material';
+import { Stack, Container, InputAdornment, IconButton } from '@mui/material';
+import { useState } from 'react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 interface LoginFormData {
   username: string;
@@ -11,6 +14,17 @@ interface LoginFormData {
 }
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  //ask guy why is this necessary, because even when deleted i see no difference
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
   const {
     register,
     handleSubmit,
@@ -18,6 +32,21 @@ const Login = () => {
   } = useForm<LoginFormData>();
 
   console.log(errors);
+
+  const iconAdornment = {
+    endAdornment: (
+      <InputAdornment position="end">
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={handleClickShowPassword}
+          onMouseDown={handleMouseDownPassword}
+          edge="end"
+        >
+          {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+        </IconButton>
+      </InputAdornment>
+    ),
+  };
 
   return (
     <Container maxWidth="sm" sx={{ mt: 2 }}>
@@ -43,7 +72,8 @@ const Login = () => {
           required
           {...register('password', { required: 'Password required' })}
           label="Password"
-          type="password"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={iconAdornment}
         />
         <Button type="submit" variant="contained">
           Sign In
