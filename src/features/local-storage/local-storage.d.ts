d8fb72c043d47e9b2b76da89071258cf60a32edf
Guy Tsitsiashvili <guygool4@gmail.com>
2023-01-27 03:44:10 +0200

    feat(local-storage): add local-storage handler class

---
+++ src/features/local-storage/local-storage.d.ts
@@ -0,0 +1,6 @@
+/**
+ * This will be used to override the default type of LocalStorageData to extend it given any use case.
+ */
+declare module 'local-storage-data' {
+  interface LocalStorageData {}
+}
