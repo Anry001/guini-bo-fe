ba4f40e49e53d2770ce7940f71c4d6f3257aabc5
anry <anryclass@gmail.com>
2023-02-08 22:03:58 +0200

    feat(test-commit): test for guy

--- src/features/users/api/getUsers.ts
+++ src/features/users/api/getUsers.ts
@@ -3,7 +3,13 @@ import { MutationConfig } from '@/lib/react-query';
 import { useMutation } from '@tanstack/react-query';
 import { ResponseData } from '../types';
 
-const getUsers = async () => {
+// {q} ask guy if i need to use react-query
+// eslint-disable-next-line import/prefer-default-export
+export const getUsers = async () => {
   const res = await apiRequest.get<ResponseData>('/admin/api/users');
-  return res.data;
+  return res.data; // {q} should i make a method that converts the response and returns the converted value?
+  // i need for each user in the response to put in a new object.
 };
+
+// {q} if ill use react-query do i need to make a try and catch outside of this file when ill use this file as a hook?
+//
--- src/features/users/components/UsersList.tsx
+++ src/features/users/components/UsersList.tsx
@@ -1,7 +1,71 @@
+import apiRequest from '@utils/apiRequest';
 import Box from '@mui/material/Box';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { keys } from 'ts-transformer-keys';
+import { ResponseData } from '../types';
+// import { getUsers } from '../api/getUsers';
 
-const columns: GridColDef[] = [
+type UserStatus =
+  | 'UNCONFIRMED'
+  | 'CONFIRMED'
+  | 'EXTERNAL_PROVIDER'
+  | 'ARCHIVED'
+  | 'UNKNOWN'
+  | 'RESET_REQUIRED'
+  | 'FORCE_CHANGE_PASSWORD';
+
+interface User {
+  email: string;
+  emailVerified: boolean;
+  firstName: string;
+  lastName: string;
+  phoneNumber: boolean;
+  phoneVerified: boolean;
+  userStatus: UserStatus;
+  username: string;
+}
+
+const getUsers = async () => {
+  const res = await apiRequest.get<ResponseData>('/admin/api/users');
+  // return res.data;
+  const keyColumns = keys<User>();
+
+  const columns: GridColDef[] = [
+    keyColumns.map((key) => {( field: key, headerName: key, width: 90 )}),
+  ];
+};
+// i want to return an array of objects that each object
+
+// {q} should i make a method that converts the response and returns the converted value?
+// i need for each user in the response to put in a new object.
+
+/* const convert = () => {
+  const [data]: ResponseData[] = getUsers();
+  const columns: GridColDef[] = [data.forEach(element => {
+
+  });]
+};
+ */
+
+const UsersList = () => {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
+  );
+};
+
+export default UsersList;
+
+const columns: GridColDef<User>[] = [
   { field: 'id', headerName: 'ID', width: 90 },
   {
     field: 'firstName',
@@ -45,20 +109,54 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-const UsersList = () => {
-  return (
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
-      />
-    </Box>
-  );
-};
+interface RawData {
+  prop1: number;
+  prop2: string;
+  prop3: {
+    prop4: number;
+    prop5: string;
+  };
+  prop6: {
+    prop7: number;
+    prop8: string;
+    prop9: {
+      prop10: number;
+      prop11: string;
+    };
+  };
+}
 
-export default UsersList;
+interface FlattenedData {
+  prop1: number;
+  prop2: string;
+  prop3Prop4: number;
+  prop3Prop5: string;
+  prop6Prop7: number;
+  prop6Prop8: string;
+  prop6Prop9Prop10: number;
+  prop6Prop9Prop11: string;
+}
+
+const flatten = ({
+  prop1,
+  prop2,
+  prop3: { prop4, prop5 },
+  prop6: {
+    prop7,
+    prop8,
+    prop9: { prop10, prop11 },
+  },
+}: RawData): FlattenedData => ({
+  prop1,
+  prop2,
+  prop3Prop4: prop4,
+  prop3Prop5: prop5,
+  prop6Prop7: prop7,
+  prop6Prop8: prop8,
+  prop6Prop9Prop10: prop10,
+  prop6Prop9Prop11: prop11,
+});
+
+const convert = (dataArray: RawData[]): FlattenedData[] => {
+  return dataArray.map(flatten);
+};
