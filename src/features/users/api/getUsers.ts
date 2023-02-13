1dc27e68ca11e4dc308a80e341a13243b63141d2
anry <anryclass@gmail.com>
2023-02-13 19:41:57 +0200

    feat(get-users): implemented the get api with react query

--- src/features/users/api/getUsers.ts
+++ src/features/users/api/getUsers.ts
@@ -1,13 +1,23 @@
 import apiRequest from '@utils/apiRequest';
+import { useQuery } from '@tanstack/react-query';
+import { ExtractFnReturnType, QueryConfig } from '@/lib/react-query';
 import { User } from '../types';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getUsers = async () => {
   const res = await apiRequest.get<User[]>('/admin/api/users');
   return res.data;
-  // return res.data; // {q} should i make a method that converts the response and returns the converted value?
-  // i need for each user in the response to put in a new object.
 };
 
-// {q} if ill use react-query do i need to make a try and catch outside of this file when ill use this file as a hook?
-//
+type QueryFnType = typeof getUsers;
+
+type UseUsersOptions = {
+  config?: QueryConfig<QueryFnType>;
+};
+
+export const useUsers = ({ config }: UseUsersOptions = {}) => {
+  return useQuery<ExtractFnReturnType<QueryFnType>>({
+    queryKey: ['users'],
+    queryFn: getUsers,
+    ...config,
+  });
+};
