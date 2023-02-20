dac7eab727ea0c87c34853a23ebcb65c5844be56
anry <anryclass@gmail.com>
2023-02-20 19:08:58 +0200

    build(index): added response interface

--- src/features/users/types/index.ts
+++ src/features/users/types/index.ts
@@ -17,3 +17,27 @@ export interface User {
   userStatus: UserStatus;
   username: string;
 }
+
+interface LocationData {
+  address: string;
+  geocode: { lat: number; lng: number };
+  placeId: string;
+}
+
+export interface PartnerStore {
+  businessCity: string;
+  businessLocation: LocationData;
+  businessName: string;
+  description: string;
+  freeWorkingDistance: number;
+  isMobile: boolean;
+  maxWorkingDistance: number;
+  partnerId: string;
+  partnerUsername: string;
+  pictures: string[];
+  pricePerKm: string;
+  profilePicture: string;
+  shortestNotice: string;
+}
+
+// type ResponseData = PartnerStore[];
