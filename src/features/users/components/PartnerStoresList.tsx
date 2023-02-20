28479b798a926f53919f2c9605cf83caaec73f8a
anry <anryclass@gmail.com>
2023-02-20 19:14:11 +0200

    feat(partner-stores-list): added table with all partner stores

---
+++ src/features/users/components/PartnerStoresList.tsx
@@ -0,0 +1,103 @@
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { usePartnerStores } from '../api/getPartnerStores';
+import { PartnerStore } from '../types';
+
+const columns: GridColDef<PartnerStore>[] = [
+  { field: 'businessCity', headerName: 'Business city', width: 200 },
+  {
+    field: 'address',
+    headerName: 'Address',
+    width: 115,
+  },
+  {
+    field: 'lat',
+    headerName: 'Latitude',
+    width: 100,
+  },
+  {
+    field: 'lng',
+    headerName: 'Longitude',
+    width: 120,
+  },
+  {
+    field: 'placeId',
+    headerName: 'Place ID',
+    width: 125,
+  },
+  {
+    field: 'businessName',
+    headerName: 'Business Name',
+    width: 110,
+  },
+  {
+    field: 'description',
+    headerName: 'Description',
+    width: 220,
+  },
+  {
+    field: 'freeWorkingDistance',
+    headerName: 'Free Working Distance',
+    width: 250,
+  },
+  {
+    field: 'isMobile',
+    headerName: 'Is Mobile',
+    width: 250,
+  },
+  {
+    field: 'maxWorkingDistance',
+    headerName: 'Max Working Distance',
+    width: 250,
+  },
+  {
+    field: 'partnerId',
+    headerName: 'Partner Id',
+    width: 250,
+  },
+  {
+    field: 'partnerUsername',
+    headerName: 'Partner Username',
+    width: 250,
+  },
+  {
+    field: 'pictures',
+    headerName: 'Pictures',
+    width: 250,
+  },
+  {
+    field: 'pricePerKm',
+    headerName: 'Price Per Km',
+    width: 250,
+  },
+  {
+    field: 'profilePicture',
+    headerName: 'Profile Picture',
+    width: 250,
+  },
+  {
+    field: 'shortestNotice',
+    headerName: 'Shortest Notice',
+    width: 250,
+  },
+];
+
+const PartnerStoresList = () => {
+  const { data = [], isLoading } = usePartnerStores();
+
+  return (
+    <Box sx={{ height: 'calc(100vh - 82px)', width: '100%' }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        getRowId={(row) => row.partnerId}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+        loading={isLoading}
+      />
+    </Box>
+  );
+};
+
+export default PartnerStoresList;
